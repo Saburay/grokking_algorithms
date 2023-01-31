a826122402107894f49b6a5645501b413b9ee493
Ra <saburay32@yandex.ru>
2023-01-31 14:40:21 +0300

    add step-count

--- main.py
+++ main.py
@@ -9,18 +9,22 @@ from random import sample
 def binary_search(list,item):
     low = 0
     high = len(list)-1
+    count = 0
 
     while low<=high:
         mid = int((low+high)/2)
         guess = list[mid]
+        count+=1
         if guess == item:
+            print(f'{count} step for search')
             return mid+1
         if guess > item:
             high = mid-1
         else:
             low = mid+1
+    print(f'{count} step for search')
     return None
 
-m_list = sorted(sample(range(1, 101), 100))
+m_list = sorted(sample(range(1, 129), 128))
 
-print(binary_search(m_list,100))
+print(binary_search(m_list,127))
